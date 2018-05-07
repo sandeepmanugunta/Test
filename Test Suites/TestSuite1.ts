a9959e329ee516f6b5f91a1f542fddf15851ccc8
smanugunta <smanugunta@USR-SMANUGUNTA1.avoka.local>
2018-05-07 10:58:51 +1000

    Added

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-07T10:56:51</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>65b7f09a-7aad-40a9-9f5f-afb66eff91c8</testSuiteGuid>
+   <testCaseLink>
+      <guid>4c1d7c34-cc85-4d6e-9a9e-f7cc071a4e42</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Testcase 1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
